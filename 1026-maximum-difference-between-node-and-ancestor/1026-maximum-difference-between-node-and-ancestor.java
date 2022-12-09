cb69a47b374f06a18af0b6125a44cfc80aed5817
Hriday Kumar Pranit <116939133+Hridaykp@users.noreply.github.com>
2022-12-09 15:07:51 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (84.67%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -1,27 +1,16 @@
 class Solution {
-    int res ;
-    private int[] helper(TreeNode root){
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        if(root == null) return new int[]{min, max};
-        if(root.left == null && root.right == null){
-            min = root.val;
-            max = root.val;
-            return new int[]{min, max};
-        }
-        int left[] = helper(root.left);
-        int right[] = helper(root.right);
-        min = Math.min(left[0], right[0]);
-        max = Math.max(left[1], right[1]);
-        res = Math.max(res, Math.max(Math.abs(min-root.val), Math.abs(max-root.val)));
+    int res;
+    private void helper(TreeNode root, int min, int max){
+        if(root == null) return;
         min = Math.min(min, root.val);
         max = Math.max(max, root.val);
-        return new int []{min, max};
+        res = Math.max(res, Math.abs(min-max));
+        helper(root.left, min, max);
+        helper(root.right, min, max);
         
     }
     public int maxAncestorDiff(TreeNode root) {
-        res = 0;
-        helper(root);
+        helper(root, root.val, root.val);
         return res;
     }
 }
