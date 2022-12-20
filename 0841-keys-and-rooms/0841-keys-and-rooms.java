02c4735b18c3e121e0edfc8bb23e8fc48423d3fb
Hriday Kumar Pranit <116939133+Hridaykp@users.noreply.github.com>
2022-12-20 22:40:12 +0530

    Time: 2 ms (70.49%), Space: 44.7 MB (46.47%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,23 +1,15 @@
 class Solution {
+    private void hasKey(List<List<Integer>> rooms, boolean[]seen, int room){
+        seen[room] = true;
+        for(int i: rooms.get(room))
+            if(seen[i] == false)
+                hasKey(rooms, seen, i);
+    }
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-        Stack<Integer> stk = new Stack();
-        boolean visited[] = new boolean[rooms.size()];
-        visited[0] = true;
-        for(int i: rooms.get(0)){
-            stk.push(i);
-            visited[i] = true;
-        }
-        while(!stk.isEmpty()){
-            int temp = stk.pop();
-            for(int i: rooms.get(temp)){
-                if(visited[i] != true){
-                    stk.push(i);
-                    visited[i] = true;
-                }
-            }
-        }
-        for(boolean flag: visited)
-            if(flag == false)
+        boolean[]seen = new boolean[rooms.size()];
+        hasKey(rooms, seen, 0);
+        for(boolean flag: seen)
+            if(!flag)
                 return false;
         return true;
     }
