cbbbf19e12e8b114bf0587ee3d2b57bc995f4dd2
Hriday Kumar Pranit <116939133+Hridaykp@users.noreply.github.com>
2022-12-24 00:46:16 +0530

    Time: 12 ms (13.30%), Space: 41.9 MB (68.62%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,12 +1,25 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-    int sell = 0, prev_sell = 0, buy = Integer.MIN_VALUE, prev_buy;
-    for (int price : prices) {
-        prev_buy = buy;
-        buy = Math.max(prev_sell - price, prev_buy);
-        prev_sell = sell;
-        sell = Math.max(prev_buy + price, prev_sell);
+    private int solver(int[] prices, int idx, int sellOrBuy, Map<String,Integer>map){
+        if(idx >= prices.length) return 0;
+        String key = sellOrBuy+"trading"+idx;
+        if(map.containsKey(key))
+            return map.get(key);
+         int profit = 0;
+        if(sellOrBuy == 0){
+            int buy = solver(prices, idx + 1, 1, map) - prices[idx];
+            int no_buy = solver(prices, idx + 1, 0, map);
+            profit = Math.max(buy, no_buy);
+        }
+        else{
+            int sell = solver(prices, idx + 2, 0, map) + prices[idx];
+            int no_sell = solver(prices, idx + 1, 1, map);
+            profit = Math.max(sell, no_sell);
+        }
+        map.put(key, profit);
+        return profit;
     }
-    return sell;
+    public int maxProfit(int[] prices) {
+        Map<String,Integer> map = new HashMap();
+        return solver(prices, 0, 0, map);
     }
 }
