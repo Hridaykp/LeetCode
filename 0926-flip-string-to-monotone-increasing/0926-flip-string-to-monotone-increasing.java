f32df91b86f68c8351e24f9c1836d7df4fac7047
Hriday Kumar Pranit <116939133+Hridaykp@users.noreply.github.com>
2023-01-17 23:52:53 +0530

    Time: 8 ms (96.61%), Space: 43.1 MB (55.59%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.java
@@ -3,10 +3,10 @@ class Solution {
         int count_1s = 0, changes = 0;
         for(char ch: s.toCharArray()){
             if(ch == '0'){
-                if(count_1s == 0)
+                if(count_1s == 0) 
                     continue;
                 else 
-                    changes ++;  
+                    changes++; 
             }
             else{
                 count_1s ++;
