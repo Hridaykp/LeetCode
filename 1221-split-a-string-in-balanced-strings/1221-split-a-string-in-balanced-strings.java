edcf2c5f673390a5f3f4e75bee0ccc3c9c1fe71d
Hriday Kumar Pranit <116939133+Hridaykp@users.noreply.github.com>
2023-02-27 22:58:43 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (49.92%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int cnt = 0, ans = 0;
+        for(char c: s.toCharArray()){
+            if(c == 'L')
+                cnt++;
+            else{
+                cnt--;
+            }
+            if(cnt == 0)ans++;
+        }
+        return ans;
+    }
+}
