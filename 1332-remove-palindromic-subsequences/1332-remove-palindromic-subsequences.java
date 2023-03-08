ba7decd1f3b224d3755cbdd3099d91ba93fd3701
Hriday Kumar Pranit <116939133+Hridaykp@users.noreply.github.com>
2023-03-08 11:12:17 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (24.12%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -1,9 +1,9 @@
 class Solution {
     private boolean isPalin(String s){
-        String p = "";
+        StringBuilder p = new StringBuilder();
         for(int i=s.length()-1; i>=0; i--)
-            p += s.charAt(i);
-        return s.equals(p);
+            p.append(s.charAt(i));
+        return s.equals(p.toString());
     }
     public int removePalindromeSub(String s) {
         if(isPalin(s))
