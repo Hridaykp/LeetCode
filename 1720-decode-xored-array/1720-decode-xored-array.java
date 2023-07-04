00c1a4f2061d30e77061b17bd0acd12dde7b9cab
Hriday Kumar Pranit <116939133+Hridaykp@users.noreply.github.com>
2023-07-05 00:09:18 +0530

    Time: 1 ms (100.00%), Space: 45.2 MB (67.13%) - LeetHub

---
+++ 1720-decode-xored-array/1720-decode-xored-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        List<Integer> list = new ArrayList();
+        int arr [] = new int[encoded.length+1];
+        arr[0] = first;
+        for(int i=0; i<encoded.length; i++)
+            arr[i+1] = arr[i]^encoded[i];
+        return arr;
+    }
+}
