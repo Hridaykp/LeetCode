a4d54b2f0f2bd193ff2359e28d0ce1a9960b191c
Hriday Kumar Pranit <116939133+Hridaykp@users.noreply.github.com>
2026-06-14 19:19:42 +0530

    Time: 0 ms (100%), Space: 19.4 MB (33.93%) - LeetHub

--- 3959-check-good-integer/3959-check-good-integer.py
+++ 3959-check-good-integer/3959-check-good-integer.py
@@ -1,16 +1,10 @@
 class Solution:
     def checkGoodInteger(self, n: int) -> bool:
-        def digitSum(n: int) -> int:
-            sm = 0
-            while n != 0:
-                sm += n % 10
-                n //= 10
-            return sm
-        def digitSqrSum(n: int) -> int:
-            sq = 0
-            while n != 0:
-                sq += pow(n % 10, 2)
-                n //= 10
-            return sq
-        return digitSqrSum(n) - digitSum(n) >= 50
+        sm =  sq = 0
+        while n != 0:
+            sm += n % 10
+            sq += pow(n % 10, 2)
+            n //= 10
+         
+        return sq - sm >= 50
         
